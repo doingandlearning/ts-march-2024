17d1149a5d1303ec835d93bae0c164af5986cf1a
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-03-07 14:18:52 +0000

    adds basic web pack config

--- 08-automation/webpack.config.js
+++ 08-automation/webpack.config.js
@@ -1,8 +1,28 @@
-
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { join, resolve } = require("node:path")
 
 module.exports = {
 	entry: "./src/app.ts",
 	resolve: {
-		extensions: []
-	}
+		extensions: [".ts", ".js", ".jsx", ".tsx"]
+	}, /* compile the ts,  */
+	module: {
+		rules: [
+			{
+				test: /.tsx?$/,
+				use: "ts-loader"
+			}
+		]
+	},
+	output: {
+		filename: "bundle.js",
+		path: join(__dirname, "dist"),
+		clean: true
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: resolve(__dirname, "src", "index.html")
+			// "./src/index.html"
+		})
+	]
 }
